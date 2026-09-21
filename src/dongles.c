8c161063dafd1db7cedc5e59e37bba5223981af8
Theophilus Kofi Oppong Ansah <theophiluskofioppongansah@gmail.com>
2026-09-21 17:14:51 +0200

    nada

--- src/dongles.c
+++ src/dongles.c
@@ -14,27 +14,31 @@
 
 int	take_dongle(t_dongle *dongle)
 {
-	pthread_mutex_lock(&dongle->state_lock);
-	while (dongle->state == TAKEN)
-		pthread_cond_wait(&dongle->available_cond, &dongle->state_lock);
-	while (dongle->state == COOLDOWN)
+	while (dongle->state != FREE)
 	{
-		clock_gettime(CLOCK_REALTIME, &dongle->now);
-		dongle->target_time = dongle->last_release;
-		dongle->target_time.tv_nsec += dongle->global->dongle_cooldown * 1000000;
+		pthread_mutex_lock(&dongle->state_lock);
+		if (dongle->state == TAKEN)
+			pthread_cond_wait(&dongle->available_cond, &dongle->state_lock);
+		else if (dongle->state == COOLDOWN)
 		{
-			if (dongle->target_time.tv_nsec >= 1000000000)
+			clock_gettime(CLOCK_REALTIME, &dongle->now);
+			dongle->target_time = dongle->last_release;
+			dongle->target_time.tv_nsec += dongle->global->dongle_cooldown * 1000000;
 			{
-				dongle->target_time.tv_sec += dongle->target_time.tv_nsec / 1000000000;
-				dongle->target_time.tv_nsec %= 1000000000;
+				if (dongle->target_time.tv_nsec >= 1000000000)
+				{
+					dongle->target_time.tv_sec += dongle->target_time.tv_nsec / 1000000000;
+					dongle->target_time.tv_nsec %= 1000000000
+					;
+				}
+				if (dongle->now.tv_sec < dongle->target_time.tv_sec ||
+					(dongle->now.tv_sec == dongle->target_time.tv_sec &&
+					dongle->now.tv_nsec < dongle->target_time.tv_nsec ))
+					pthread_cond_timedwait(&dongle->available_cond, &dongle->state_lock,
+					&dongle->target_time);
+				else
+					dongle->state = FREE;
 			}
-			if (dongle->now.tv_sec < dongle->target_time.tv_sec ||
-				(dongle->now.tv_sec == dongle->target_time.tv_sec &&
-				dongle->now.tv_nsec < dongle->target_time.tv_nsec ))
-				pthread_cond_timedwait(&dongle->available_cond, &dongle->state_lock,
-				&dongle->target_time);
-			else
-				dongle->state = FREE;
 		}
 	}
 	dongle->state = TAKEN;
@@ -46,6 +50,7 @@ int	drop_dongle(t_dongle *dongle)
 {
 	pthread_mutex_lock(&dongle->state_lock);
 	dongle->state = COOLDOWN;
+	clock_gettime(CLOCK_REALTIME, &dongle->last_release);
 	pthread_cond_signal(&dongle->available_cond);
 	pthread_mutex_unlock(&dongle->state_lock);
 	return (1);
@@ -53,16 +58,45 @@ int	drop_dongle(t_dongle *dongle)
 
 void	acquire_dongles(t_coder *coder)
 {
+	t_global *global;
+
+    global = coder->left_dongle->global;
 	if (coder->id % 2 == 0)
 	{
+		pthread_mutex_lock(&global->log_mutex);
+        fprintf(stderr, "[Coder %d] Esperando dongle izq #%td\n",
+            coder->id, coder->left_dongle - global->dongles);
+        pthread_mutex_unlock(&global->log_mutex);
+		
 		take_dongle(coder->left_dongle);
+
+		pthread_mutex_lock(&global->log_mutex);
+        fprintf(stderr, "[Coder %d] Tomado izq #%td. Esperando der #%td\n",
+            coder->id, coder->left_dongle - global->dongles, coder->right_dongle - global->dongles);
+        pthread_mutex_unlock(&global->log_mutex);
+		
 		take_dongle(coder->right_dongle);
 	}
 	else
 	{
+		pthread_mutex_lock(&global->log_mutex);
+        fprintf(stderr, "[Coder %d] Esperando dongle der #%td\n",
+            coder->id, coder->right_dongle - global->dongles);
+        pthread_mutex_unlock(&global->log_mutex);
+		
 		take_dongle(coder->right_dongle);
+		
+		pthread_mutex_lock(&global->log_mutex);
+        fprintf(stderr, "[Coder %d] Tomado der #%td. Esperando izq #%td\n",
+            coder->id, coder->right_dongle - global->dongles, coder->left_dongle - global->dongles);
+        pthread_mutex_unlock(&global->log_mutex);
+		
 		take_dongle(coder->left_dongle);
 	}
+	pthread_mutex_lock(&global->log_mutex);
+    fprintf(stderr, "[Coder %d] AMBOS DONGLES CONSEGUIDOS (#%td y #%td)\n",
+        coder->id, coder->left_dongle - global->dongles, coder->right_dongle - global->dongles);
+    pthread_mutex_unlock(&global->log_mutex);
 }
 
 int	init_dongles(t_global *global)
@@ -92,6 +126,7 @@ int	init_dongles(t_global *global)
 			global->dongles = NULL;
 			return (0);
 		}
+		global->dongles[i].global = global;
 		global->dongles[i].state = condition;
 		global->dongles[i].last_release.tv_sec = 0;
 		global->dongles[i].last_release.tv_nsec = 0;
